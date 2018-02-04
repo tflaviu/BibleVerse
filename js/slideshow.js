c8ef6419edba5a151ee73c5227cb359a29712d77
tflaviu <turcut.flv@gmail.com>
2018-02-04 16:09:55 +0200

    added slideshow //click on image and drag to right or left, for image to change

---
+++ js/slideshow.js
@@ -0,0 +1,14 @@
+//$('.image_container').slick({
+//  slidesToShow: 3,
+//  slidesToScroll: 1,
+//  autoplay: true,
+//  autoplaySpeed: 2000,
+//});
+
+//
+//$('.image_container').slick({
+//  slidesToShow: 3,
+//  slidesToScroll: 1,
+//  autoplay: true,
+//  autoplaySpeed: 2000,
+//});
